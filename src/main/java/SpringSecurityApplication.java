8b4af4dff423e20bc6f43fd411d4ded5da09c9cd
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-09-02 15:15:02 +0530

    Import SpringApplication in SpringSecurityApplication
    Added SpringApplication import for bootstrapping.

--- src/main/java/SpringSecurityApplication.java
+++ src/main/java/SpringSecurityApplication.java
@@ -1,5 +1,8 @@
 package com.spring.springSecurity;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class SpringSecurityApplication {
   
