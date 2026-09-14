33a1d6c7fa76cc19035c87ce90b13ef60a1ef6cf
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-09-14 17:26:23 +0530

    Import Spring Security classes in helloController
    Added necessary imports for Spring Security features.

--- src/main/java/helloController.java
+++ src/main/java/helloController.java
@@ -1,5 +1,12 @@
 package com.spring.springSecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+
 @RestController
 public class helloController {
   
