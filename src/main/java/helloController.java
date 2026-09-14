1edc3592a027624aaa12a1003c8df062fde3cf1f
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-09-14 17:28:05 +0530

    Update in helloController

--- src/main/java/helloController.java
+++ src/main/java/helloController.java
@@ -1,6 +1,6 @@
 package com.spring.springSecurity;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
